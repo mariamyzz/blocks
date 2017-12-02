a1304b524b89fe73e33cd0f4971dcdc64e760cbf
Andrey Ermilov <andrerm@ya.ru>
2017-12-02 10:21:59 +0300

    Renames base project to server

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "parentproject.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings.dev")
 
     from django.core.management import execute_from_command_line
 
